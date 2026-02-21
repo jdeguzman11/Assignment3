d680771f0bb149a42778f33e8a665f34f2e1bceb
Justin DeGuzman <justindeguzman@dhcp-10-8-015-144.mobile.reshsg.uci.edu>
2026-02-20 16:45:41 -0800

    Finished ds_client and ds_protocol

--- ds_client.py
+++ ds_client.py
@@ -4,16 +4,57 @@
 # justicd1@uci.edu
 # 72329664
 
-def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
-  '''
-  The send function joins a ds server and sends a message, bio, or both
+import socket
+from typing import Optional
+import json
 
-  :param server: The ip address for the ICS 32 DS server.
-  :param port: The port where the ICS 32 DS server is accepting connections.
-  :param username: The user name to be assigned to the message.
-  :param password: The password associated with the username.
-  :param message: The message to be sent to the server.
-  :param bio: Optional, a bio for the user.
-  '''
-  #TODO: return either True or False depending on results of required operation
-  return 
+
+def _connect(host: str, port: int) -> Optional[socket.socket]:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        return s
+    except Exception:
+        return None
+
+
+def _send_json(sock: socket.socket, message: dict) -> bool:
+    try:
+        data = json.dummps(message).encode("utf-8")
+        sock.sendall(data)
+        return True
+    except Exception:
+        return False
+
+
+def send(
+    server: str,
+    port: int,
+    username: str,
+    password: str,
+    message: str,
+    bio: str = None
+) -> bool:
+    '''
+    The send function joins a ds server and sends a message, bio, or both
+    :param server: The ip address for the ICS 32 DS server.
+    :param port: The port where the ICS 32 DS server is accepting connections.
+    :param username: The user name to be assigned to the message.
+    :param password: The password associated with the username.
+    :param message: The message to be sent to the server.
+    :param bio: Optional, a bio for the user.
+    '''
+    sock = _connect(server, port)
+    if sock is None:
+        return False
+
+    try:
+        if not _send_json(sock, message):
+            return False
+    finally:
+        try:
+            sock.close()
+        except Exception:
+            pass
+
+    return True
--- ds_protocol.py
+++ ds_protocol.py
@@ -7,21 +7,16 @@
 import json
 from collections import namedtuple
 
-# Namedtuple to hold the values retrieved from json messages.
-# TODO: update this named tuple to use DSP protocol keys
-DataTuple = namedtuple('DataTuple', ['foo','baz'])
+DataTuple = namedtuple("DateTuple", ["type", "token"])
 
-def extract_json(json_msg:str) -> DataTuple:
-  '''
-  Call the json.loads function on a json string and convert it to a DataTuple object
-  
-  TODO: replace the pseudo placeholder keys with actual DSP protocol keys
-  '''
-  try:
-    json_obj = json.loads(json_msg)
-    foo = json_obj['foo']
-    baz = json_obj['bar']['baz']
-  except json.JSONDecodeError:
-    print("Json cannot be decoded.")
 
-  return DataTuple(foo, baz)
+def extract_json(json_msg: str) -> DataTuple:
+    try:
+        json_obj = json.loads(json_msg)
+        type = json_obj["response"]["type"]
+        token = json_obj["response"]["message"]
+    except json.JSONDecodeError:
+        print("Json cannot be decoded.")
+        return DataTuple(None, None)
+
+    return DataTuple(type, token)
